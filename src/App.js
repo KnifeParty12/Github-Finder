8240fcbffc5f07bf806db325a8fb69910642c3c5
KnifeParty12 <flashfunk01@gmail.com>
2019-07-15 19:46:22 +0530

    Created a UserItem
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 class App extends Component {
     render(){
     return (
         <div className="App">
           <Navbar title ="Github Finder" icon='fab fa-github'/>
+          <UserItem/>
         </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Navbar extends Component {
     static defaultProps = {
         title: 'Github Finder',
         icon: 'fab fa-github'
+    };
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
     };
 
     render() {
