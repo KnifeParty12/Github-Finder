1424033e8a51461ec4728f1f301a1bd6b5595c26
KnifeParty12 <flashfunk01@gmail.com>
2019-07-15 18:25:13 +0530

    Added Navbar
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
-
+import Navbar from './components/layout/Navbar';
 class App extends Component {
     render(){
     return (
         <div className="App">
-          <h1> Hello From React </h1>
+          <Navbar title ="Github Finder" icon='fab fa-github'/>
         </div>
     );
   }
