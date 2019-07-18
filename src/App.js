957e1d7a17be414a43a32989f719e12f0d9376f7
KnifeParty12 <flashfunk01@gmail.com>
2019-07-18 14:53:43 +0530

    Moved to functional components
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 class App extends Component {
     render(){
     return (
         <div className="App">
           <Navbar title ="Github Finder" icon='fab fa-github'/>
-          <UserItem/>
+          <Users/>
         </div>
     );
   }
