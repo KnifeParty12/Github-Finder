274eddebb02974f31887fcb891f872b8a2928a0a
KnifeParty12 <flashfunk01@gmail.com>
2019-07-18 20:03:46 +0530

    Link to home and about
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from  './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 
 class App extends Component {
     state = {
         users: [],
+        user: {},
         loading: false,
         alert: null
     };
@@ -16,11 +19,23 @@ class App extends Component {
     // Search Github Users
     searchUsers = async (text) =>{
         this.setState({loading : true});
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
+        const res = await axios.get(
+            `https://api.github.com/search/users?q=${text}&client_id=$
          {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
          {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
         this.setState({users : res.data.items, loading: false});
     };
+
+    // Get A single Github User
+    getUser = async (username) => {
+        this.setState({loading : true});
+        const res = await axios.get(
+            `https://api.github.com/users/${username}?client_id=$
+         {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+         {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        this.setState({user : res.data, loading: false});
+    };
+
 
     // CLear Users from state
     clearUsers = () => this.setState({users: [] , loading: false});
@@ -34,20 +49,31 @@ class App extends Component {
 
     render(){
         const {users, loading} = this.state;
+
     return (
+        <Router>
         <div className="App">
           <Navbar title ="Github Finder" icon='fab fa-github'/>
           <div className="container">
               <Alert alert={this.state.alert}/>
-              <Search
-                  searchUsers ={this.searchUsers}
-                  clearUsers={this.clearUsers}
-                  showClear={users.length > 0 ? true :false}
-                  setAlert={this.setAlert}
-                    />
-              <Users loading ={loading}  users = {users}/>
+              <Switch>
+                  <Route exact path='/' render={props =>(
+                      <Fragment>
+                          <Search
+                              searchUsers ={this.searchUsers}
+                              clearUsers={this.clearUsers}
+                              showClear={users.length > 0 ? true :false}
+                              setAlert={this.setAlert}
+                          />
+                          <Users loading ={loading}  users = {users}/>
+                      </Fragment>
+                      )}/>
+                      <Route exact path= '/about' component={About}/>
+              </Switch>
+
           </div>
         </div>
+        </Router>
     );
   }
 }
