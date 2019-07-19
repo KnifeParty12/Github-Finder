2286ac75b625401e0d253eafd97ad266f3920e79
KnifeParty12 <flashfunk01@gmail.com>
2019-07-20 00:08:57 +0530

    Added User UI
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component, Fragment} from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import User from './components/users/User';
@@ -8,13 +7,16 @@ import Search from './components/users/Search';
 import Alert from  './components/layout/Alert';
 import About from './components/pages/About';
 import axios from 'axios';
+import './App.css';
+
 
 class App extends Component {
     state = {
         users: [],
         user: {},
         loading: false,
-        alert: null
+        alert: null,
+        repos: []
     };
 
     // Search Github Users
@@ -37,6 +39,17 @@ class App extends Component {
         this.setState({user : res.data, loading: false});
     };
 
+    // Get User's top 5 Repos
+    getUserRepos = async (username) => {
+        this.setState({loading : true});
+        const res = await axios.get(
+            `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+         {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+         {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        this.setState({repos : res.data, loading: false});
+    };
+
+
 
     // CLear Users from state
     clearUsers = () => this.setState({users: [] , loading: false});
@@ -49,7 +62,7 @@ class App extends Component {
 };
 
     render(){
-        const {users, user,loading} = this.state;
+        const {users, user,repos,loading} = this.state;
 
     return (
         <Router>
@@ -73,7 +86,9 @@ class App extends Component {
                       <Route exact path='/user/:login' render={props =>(
                        <User {...props}
                              getUser={this.getUser}
+                             getUserRepos={this.getUserRepos}
                              user={user}
+                             repos={repos}
                              loading={loading}/>
                       )}/>
               </Switch>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,19 @@
-import React, {Component} from 'react';
-
+import React, {Fragment, Component} from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
+        this.props.getUserRepos(this.props.match.params.login);
     }
+
+    static propTypes = {
+      loading : PropTypes.bool.isRequired,
+      user : PropTypes.object.isRequired,
+      getUser : PropTypes.func.isRequired,
+      getUserRepos: PropTypes.func.isRequired,
+    };
 
     render() {
         const {
@@ -12,6 +22,7 @@ class User extends Component {
             location,
             bio,
             blog,
+            company,
             login,
             html_url,
             followers,
@@ -22,7 +33,63 @@ class User extends Component {
         } = this.props.user;
         const {loading} = this.props;
 
-        return<div> {name} </div>
+        if(loading) return <Spinner/>;
+
+        return  <Fragment>
+            <Link to="/" className = 'btn btn-light'>
+            Back to Search
+            </Link>
+            Hireable: {' '}
+            {hireable ? (
+                <i className= "fas fa-check text-success"/>
+                ) : (
+                <i className= "fas fa-times-circle text-danger" /> )}
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url}  className="round-img" alt=""
+                             style={{width: '150px'}}
+                        />
+                        <h1>{name}</h1>
+                        <p>Location : {location}</p>
+                    </div>
+                    <div>
+                        {bio && (<Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                        )}
+
+                        <a href={html_url} className="btn btn-dark my-1"> Visit Github Profile
+                        </a>
+                       <ul>
+                        <li>
+                            {login && <Fragment>
+                                <strong> Username:</strong> {login}
+                            </Fragment>}
+                        </li>
+
+                           <li>
+                               {company && <Fragment>
+                                   <strong> Company:</strong> {company}
+                               </Fragment>}
+                           </li>
+                           <li>
+                               {blog && <Fragment>
+                                   <strong> Website:</strong> {blog}
+                               </Fragment>}
+                           </li>
+                       </ul>
+                    </div>
+                </div>
+            <div className="card text-center">
+                <div className="badge badge-primary"> Followers: {followers}</div>
+                <div className="badge badge-success"> Followers: {following}</div>
+                <div className="badge badge-list"> Followers: {public_repos}</div>
+                <div className="badge badge-dark"> Followers: {public_gists}</div>
+            </div>
+        </Fragment>;
+
+        return <div> {name} </div>
     }
 }
 
