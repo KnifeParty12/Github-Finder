aa6cae4230d581d4426c3717dd781b48410e3eec
KnifeParty12 <flashfunk01@gmail.com>
2019-07-20 23:25:44 +0530

    Added Home Component seperately
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, {Fragment} from 'react';
+ import React, {Fragment} from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import Alert from  './components/layout/Alert';
 import About from './components/pages/About';
 
@@ -24,12 +23,7 @@ const App = () => {
           <div className="container">
               <Alert alert={alert}/>
               <Switch>
-                  <Route exact path='/' render={props =>(
-                      <Fragment>
-                          <Search/>
-                          <Users />
-                      </Fragment>
-                      )}/>
+                  <Route exact path='/' component={Home}/>
                       <Route exact path= '/about' component={About}/>
                       <Route exact path='/user/:login' component={User}/>
               </Switch>
